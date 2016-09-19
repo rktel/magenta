945536d409c70481951503cbafb250b593266cba
rktel <rakinakuy@hotmail.com>
2016-09-19 18:28:00 -0500

    nata list devices Online

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -8,9 +8,15 @@
  * For more information on bootstrapping your app, check out:
  * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.bootstrap.html
  */
+var Device = require('nata-device');
 
 module.exports.bootstrap = function(cb) {
-
+const deviceID = 'MVKZS8YH8SLN4SWO';
+//const device = new Device(deviceID);
+//const component = 'SamplePackage/.sampleActivity';
+Device.getOnlineDevices().then((it)=>{
+  console.log(it);
+})
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
   cb();
